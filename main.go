899bca56ff7ab43d0e671ee5f249b35f9fdf5254
Zhi Qi <qizhi@pingcap.com>
2022-09-13 17:53:28 +0800

    disable mpp and batch-cop

--- main.go
+++ main.go
@@ -10,33 +10,42 @@ import (
 func query(db *sql.DB, statement string, except int32, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	rows := db.QueryRow("select sleep(1)")
+	for i := 0; i < 100; i++ {
+		//startTime := time.Now()
+		rows := db.QueryRow(statement)
+		//elapsedTime := time.Since(startTime) / time.Millisecond
 
-	var res int32
-	err := rows.Scan(&res)
-	if err != nil {
-		return
+		var res int32
+		err := rows.Scan(&res)
+		if err != nil {
+			fmt.Println("scan failed:", err)
+			return
+		}
+		if res != except {
+			fmt.Printf("result not match, except %v, but got %v\n", except, res)
+		} else {
+			//fmt.Printf("result match, except %v, got %v\n", except, res)
+		}
 	}
-	if res != except {
-		fmt.Printf("result not match, except %v, but got %v\n", except, res)
-	} else {
-		fmt.Printf("result match, except %v, got %v\n", except, res)
-	}
+
 }
 
 func main() {
 	// TODO: config
-	server := "127.0.0.1:4000"
-	dbName := "test"
+	server := "172.16.4.42:23300"
+	dbName := "tpch10"
 	parallel := 100
-	statement := "select count(*) from t"
-	var res int32 = 7
+	statement := "select count(*) from part where p_name like '%green%almond%' and p_name like '%read%apple%' and p_name not like '%yellow%man%' and p_name like '%still%water%' and p_name not like '%ishar%mla%' and P_SIZE not in (11, 45, 14);"
+	var res int32 = 0
 
-	db, err := sql.Open("mysql", fmt.Sprintf("root@tcp(%v)/%v?charset=utf8&parseTime=True", server, dbName))
+	db, err := sql.Open("mysql", fmt.Sprintf("root@tcp(%v)/%v?charset=utf8&parseTime=True&tidb_allow_mpp=false&tidb_allow_batch_cop=false", server, dbName))
 	if err != nil {
 		fmt.Println("database open failed:", err)
 		return
+	} else {
+		fmt.Println("database open suc")
 	}
+
 	defer func(db *sql.DB) {
 		err := db.Close()
 		if err != nil {
